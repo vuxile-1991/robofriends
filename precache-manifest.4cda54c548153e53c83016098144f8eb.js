455b4503af3213a6639650b006efd712f40e2428
unknown <vuxile@gmail.com>
2020-07-16 05:10:03 +0200

    Updates

---
+++ precache-manifest.4cda54c548153e53c83016098144f8eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5be4263ff8aba77a7649410fad91fc0",
+    "url": "/vuxile-1991/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/vuxile-1991/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f7aadd5371aa7f1d7752",
+    "url": "/vuxile-1991/robofriends/static/css/main.cf094bab.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/vuxile-1991/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vuxile-1991/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7aadd5371aa7f1d7752",
+    "url": "/vuxile-1991/robofriends/static/js/main.0c3ecd7b.chunk.js"
+  },
+  {
+    "revision": "99b00527f8aa76958683",
+    "url": "/vuxile-1991/robofriends/static/js/runtime-main.74553074.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/vuxile-1991/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
